d332a0c570b4500e145f65fe818187fa38dc8c52
jeong jaehun <145264424+ha-dong@users.noreply.github.com>
2024-08-07 17:50:09 +0900

    Add files via upload

--- ppp/mainpage/js/main.js
+++ ppp/mainpage/js/main.js
@@ -7,19 +7,39 @@ document.getElementById('dropdownButton').addEventListener('click', function() {
   }
 });
 
-document.querySelectorAll('.dropdown-item').forEach(item => {
-  item.addEventListener('click', function() {
-    window.location.href = this.getAttribute('data-href');
-  });
-});
-
-    document.getElementById('heart').addEventListener('click', function() {
-        this.classList.toggle('active'); // 클릭할 때 'active' 클래스 토글
+  document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function() {
+      window.location.href = this.getAttribute('data-href');
     });
+  });
 
-    $(document).ready(function() {
-      // 하트 버튼 클릭 시 색상 변경
-      $('.heart').click(function() {
-          $(this).toggleClass('liked');
+    document.addEventListener('DOMContentLoaded', function() {
+      // 모든 하트 버튼에 클릭 이벤트 리스너 추가
+      document.querySelectorAll('.btn.heart').forEach(function(button) {
+          button.addEventListener('click', function() {
+              // 버튼 클릭 시 'liked' 클래스를 토글
+              this.classList.toggle('liked');
+          });
       });
   });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const heartButtons = document.querySelectorAll('.btn.heart');
+    heartButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const message = document.getElementById('interestMessage');
+            if (button.classList.contains('red')) {
+                message.textContent = '관심 등록이 취소 되었습니다.';
+            } else {
+                message.textContent = '관심 등록이 되었어요.';
+            }
+            message.style.display = 'block';
+            setTimeout(() => {
+                message.style.display = 'none';
+            }, 1500);
+
+            // Toggle heart color
+            button.classList.toggle('red');
+        });
+    });
+});
