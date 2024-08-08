54991a2c8fc3f5a7a5d4c7280ff25095f49e72ba
jeong jaehun <145264424+ha-dong@users.noreply.github.com>
2024-08-08 14:57:38 +0900

    Add files via upload

---
+++ ppp/mainpage/readmore/gwansimregister/js/gwansin.js
@@ -0,0 +1,36 @@
+document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function() {
+      window.location.href = this.getAttribute('data-href');
+    });
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // 모든 하트 버튼에 클릭 이벤트 리스너 추가
+    document.querySelectorAll('.btn.heart').forEach(function(button) {
+        button.addEventListener('click', function() {
+            // 버튼 클릭 시 'liked' 클래스를 토글
+            this.classList.toggle('liked');
+        });
+    });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const heartButtons = document.querySelectorAll('.btn.heart');
+  heartButtons.forEach(button => {
+      button.addEventListener('click', function() {
+          const message = document.getElementById('interestMessage');
+          if (button.classList.contains('red')) {
+              message.textContent = '관심 등록이 등록 되었습니다.';
+          } else {
+              message.textContent = '관심 등록이 취소 되었어요.';
+          }
+          message.style.display = 'block';
+          setTimeout(() => {
+              message.style.display = 'none';
+          }, 1500);
+
+          // Toggle heart color
+          button.classList.toggle('red');
+      });
+  });
+});
