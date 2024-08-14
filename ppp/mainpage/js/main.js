4a362e060deb080b7afcfe31b1d3583c53244aee
jeong jaehun <145264424+ha-dong@users.noreply.github.com>
2024-08-14 21:25:24 +0900

    Add files via upload

--- ppp/mainpage/js/main.js
+++ ppp/mainpage/js/main.js
@@ -231,9 +231,9 @@ if (logo) {
 }
 
     // 내 정보 버튼 클릭 시 내 정보로 이동
-    const registerbtn = document.getElementById('register-btn');
-    if (registerbtn) {
-        registerbtn.addEventListener('click', function () {
+    const myinfobtn = document.getElementById('my-info-btn');
+    if (myinfobtn) {
+        myinfobtn.addEventListener('click', function () {
             window.location.href = '../html/Mypage.html'; // 수정된 경로
         });
     }
